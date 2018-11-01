9018451c04ee4b92091edd25a9d11d9d41c31276
legenda971 <legenda971@gmail.com>
2018-11-01 01:01:21 +0100

    terminal

--- terminal.c
+++ terminal.c
@@ -2,10 +2,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ncurses.h>
+#include <string.h>
 
-int main(void)
-{
+int main(void) {
+    /*
+    http://www.gnu.org/software/libc/manual/html_node/Directory-Entries.html
+
+    struct dirent {
+        ino_t          d_ino;        inode number 
+        off_t          d_off;        offset to the next dirent
+        unsigned short d_reclen;     length of this record 
+        unsigned char  d_type;       type of file; not supported
+                                       by all file system types 
+        char           d_name[256];  filename 
+    }
+    */
     struct dirent **namelist;
+
     int n;
 
     n = scandir(".", &namelist, NULL, alphasort);
@@ -21,9 +34,9 @@ int main(void)
     cbreak();
 
     int yMax, xMax;
-    getmaxyx(stdsrc, yMax, xMax);
+    getmaxyx(stdscr, yMax, xMax);
 
-    WINDOW * menuwin = newwin(6, xMax-12, yMax-8, 5);
+    WINDOW * menuwin = newwin(yMax - 1, xMax-1, 1, 1);
     box(menuwin, 0, 0);
     refresh();
     wrefresh(menuwin);
@@ -34,23 +47,38 @@ int main(void)
     int selected = 0;
 
     while(1){
-        while (n--) {
-            if(!(n - selected))
+        
+        for(int i = 0; i < n; i++){
+            if(i == selected)
                 wattron(menuwin, A_REVERSE);
-            mvwprintw(menuwin, n+1, 1, namelist[n]->d_name);
-            wattoff(menuwin, A_REVERSE);
-            free(namelist[n]);
+
+            char type[10];
+            /* TYPE */
+            #define TYPE(x) case x: strcpy(type, #x); break;
+            switch(namelist[i]->d_type){
+                TYPE(DT_DIR)
+                TYPE(DT_REG)
+
+                default:
+                  strcpy(type, "DT_DEF");
+                  break;
+            }
+            #undef TYPE
+
+            mvwprintw(menuwin, i+2, 1, "%-15s | %s |", namelist[i]->d_name, type);
+            wattroff(menuwin, A_REVERSE);
         }
-        free(namelist);
 
         choise = wgetch(menuwin);
 
         switch(choise){
-            case: KEY_UP:
-              selected--;
+            case KEY_UP:
+              if(selected > 0)
+                selected--;
               break;
-            case: KEY_DOWN:
-              selected++;
+            case KEY_DOWN:
+              if(selected < (n-1))  
+                selected++;
               break;
             default:
               break;
@@ -60,5 +88,11 @@ int main(void)
             break;
     }
 
+
+    while(n--)
+        free(namelist[n]);
+    free(namelist);
+
+    endwin();
     exit(EXIT_SUCCESS);
 }
