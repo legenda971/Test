9c305939bf0527d9bd18ee13ff92baed35a5160f
legenda971 <legenda971@gmail.com>
2018-11-11 20:12:49 +0100

    Kappa

---
+++ src/metainfo.c
@@ -0,0 +1,5 @@
+typedef struct meta_info {
+    char name[128];             // Nazov súboru
+    unsigned int lenght;        // Velkost suboru v bajtoch
+    unsigned int pieses_lenght; // Velkost jedneho kusku
+}
